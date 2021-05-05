abb33058218cc209b3e47c9efb6c66a6e2fdede0
Jeremy Banka <hello@jeremybanka.com>
2021-05-05 14:32:11 -0700

    🎨 improve code formatting
    Co-authored-by: Isaacrmoreno <ipdxcreative@gmail.com>

--- RockPaperScissors/Models/RockPaperScissors.cs
+++ RockPaperScissors/Models/RockPaperScissors.cs
@@ -7,76 +7,68 @@ namespace RockPaperScissors
   {
     public static string PlayLongform(string move0, string move1)
     {
-      switch (move0)
+      return move0 switch
       {
-        case "rock":
-          switch (move1)
-          {
-            case "rock": return "Draw!";
-            case "paper": return "Player 1 Wins!";
-            case "scissors": return "Player 0 Wins!";
-            default:
-              throw new ArgumentException("Player 1 made an invalid move!");
-          }
-        case "paper":
-          switch (move1)
-          {
-            case "rock": return "Player 0 Wins!";
-            case "paper": return "Draw!";
-            case "scissors": return "Player 1 Wins!";
-            default:
-              throw new ArgumentException("Player 1 made an invalid move!");
-          }
-        case "scissors":
-          switch (move1)
-          {
-            case "rock": return "Player 1 Wins!";
-            case "paper": return "Player 0 Wins!";
-            case "scissors": return "Draw!";
-            default:
-              throw new ArgumentException("Player 1 made an invalid move!");
-          }
-        default:
-          throw new ArgumentException("Player 0 made an invalid move!");
-      }
-
-      static int GetIdxOfMove(String[] validMoves, string move)
-      {
-        int moveIdx = Array.IndexOf(validMoves, move);
-        if (moveIdx == -1)
+        "rock" => move1 switch
         {
-          throw new ArgumentException("A player made an invalid move!");
-        }
-        return moveIdx;
-      }
-
-      static int WrapAround(int value, int min, int max)
-      {
-        int range = max - min;
-        while (value >= max) value -= range;
-        while (value < min) value += range;
-        return value;
-      }
-
-
-      static string Play(string player0move, string player1move)
-      {
-        String[] validMoves = { "rock", "paper", "scissors" };
-        if (player0move == player1move)
+          "rock" => "Draw!",
+          "paper" => "Player 1 Wins!",
+          "scissors" => "Player 0 Wins!",
+          _ => throw new ArgumentException("Player 1 made an invalid move!"),
+        },
+        "paper" => move1 switch
         {
-          return "Draw!";
-        }
-        int player0moveIdx = GetIdxOfMove(validMoves, player0move);
-        int player1moveIdx = GetIdxOfMove(validMoves, player1move);
-
-        bool player0hasWinningMove = player0moveIdx == WrapAround(player1moveIdx + 1, 0, validMoves.Length);
-        if (player0hasWinningMove)
+          "rock" => "Player 0 Wins!",
+          "paper" => "Draw!",
+          "scissors" => "Player 1 Wins!",
+          _ => throw new ArgumentException("Player 1 made an invalid move!"),
+        },
+        "scissors" => move1 switch
         {
-          return "Player 0 Wins!";
-        }
+          "rock" => "Player 1 Wins!",
+          "paper" => "Player 0 Wins!",
+          "scissors" => "Draw!",
+          _ => throw new ArgumentException("Player 1 made an invalid move!"),
+        },
+        _ => throw new ArgumentException("Player 0 made an invalid move!"),
+      };
+    }
 
-        return "foo";
+    public static int GetIdxOfMove(String[] validMoves, string move)
+    {
+      int moveIdx = Array.IndexOf(validMoves, move);
+      if (moveIdx == -1)
+      {
+        throw new ArgumentException("A player made an invalid move!");
       }
+      return moveIdx;
+    }
+
+    public static int WrapAround(int value, int min, int max)
+    {
+      int range = max - min;
+      while (value >= max) value -= range;
+      while (value < min) value += range;
+      return value;
+    }
+
+    public static string Play(string player0move, string player1move)
+    {
+      String[] validMoves = { "rock", "paper", "scissors" };
+      if (player0move == player1move)
+      {
+        return "Draw!";
+      }
+      int player0moveIdx = GetIdxOfMove(validMoves, player0move);
+      int player1moveIdx = GetIdxOfMove(validMoves, player1move);
+
+      bool player0hasWinningMove = player0moveIdx == WrapAround(player1moveIdx + 1, 0, validMoves.Length);
+      if (player0hasWinningMove)
+      {
+        return "Player 0 Wins!";
+      }
+
+      return "foo";
     }
   }
 }
